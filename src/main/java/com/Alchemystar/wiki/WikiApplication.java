956937c4407efb004739e241cbd461dbb203e940
hyuk <ppmzzng01@naver.com>
2021-07-04 18:13:39 +0900

    init

---
+++ src/main/java/com/Alchemystar/wiki/WikiApplication.java
@@ -0,0 +1,16 @@
+package com.Alchemystar.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiApplication.class, args);
+
+
+
+	}
+
+}
